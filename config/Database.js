3c57e78b44840a344515ea1b502f58a58326d506
Aldrich Santiago <aldrichsantiago20@gmail.com>
2022-12-12 16:16:01 +0800

    Changed DB Password and Frontend Site

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
  
-const db = new Sequelize('wup_scholar_db', 'root', '', {
+const db = new Sequelize('wup_scholar_db', 'root', 'aldrich123', {
     host: "localhost",
     dialect: "mysql",
     port: '3306'
